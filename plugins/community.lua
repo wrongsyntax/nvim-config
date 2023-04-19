bdcbcfd6932274faeb6ad9b04a20a07e53fa9d43
Uzair Tariq <uzair0213@icloud.com>
2023-04-19 16:24:20 -0600

    Updated colorscheme

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,26 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.colorscheme.kanagawa" },
+  {
+    "rebelot/kanagawa.nvim",
+    opts = {
+      colors = {
+        theme = {
+          all = {
+            ui = {
+              bg_gutter = "none"
+            },
+          },
+        },
+      },
+      overrides = function(colors)
+        return {
+          CursorLineNr = { fg = "#11AE91", italic = true },
+        }
+      end,
+    },
+  }
 }
