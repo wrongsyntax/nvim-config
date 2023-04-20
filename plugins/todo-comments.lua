b79389942987ac9a7ba67b82268d62e9c5e04550
Uzair Tariq <uzair0213@icloud.com>
2023-04-19 22:19:30 -0600

    Added TODO comment highlight plugin and trouble plugin

---
+++ plugins/todo-comments.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {
+      colors = {
+        error = { "#DC2626" },
+        warning = { "#FBBF24" },
+        info = { "#2563EB" },
+        hint = { "#10B981" },
+        default = { "#7C3AED" },
+        test = { "#FF00FF" },
+      },
+    },
+    event = "User AstroFile",
+  },
+}
