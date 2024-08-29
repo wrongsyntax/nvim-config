4eb09d9b83bcd7516dc0febfddd252a9c87f9adf
Uzair Tariq <uzair@wrongsyntax.dev>
2024-08-28 18:16:31 -0600

    latest

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,25 +4,16 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
-  { import = "astrocommunity.colorscheme.kanagawa-nvim" },
+  { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.colorscheme.tokyonight-nvim" },
   {
-    "rebelot/kanagawa.nvim",
+    "folke/tokyonight.nvim",
     opts = {
       transparent = true,
-      colors = {
-        theme = {
-          all = {
-            ui = {
-              bg_gutter = "none",
-            },
-          },
-        },
-      },
       overrides = function(colors)
         return {
-          CursorLineNr = { fg = "#11AE91", italic = true, bold = true },
+          -- CursorLineNr = { fg = "#11AE91", italic = true, bold = true },
+          -- colors.error = "#ff0000",
         }
       end,
     },
